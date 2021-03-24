d9be79262425af9383c9fef3f31b7376d8281560
Matt Osborn <mattjosborn@gmailcom>
2021-03-24 11:00:03 +0000

    page2 refactor

--- src/Page2.svelte
+++ src/Page2.svelte
@@ -1,36 +1,75 @@
 <script lang="ts">
     import SubNav from './components/SubNav.svelte';
-    import Land from './Page2Viz-Land.svelte';
-    import Ocean from './Page2Viz-Ocean.svelte';
-    import Fires from './Page2Viz-Fires.svelte';
+    import MainViz from './components/MainViz.svelte';
+    import CopyPane from './components/CopyPane.svelte';
+    import CopyHeadline from './components/CopyHeadline.svelte';
+    import CopyMain from './components/CopyMain.svelte';
     import ChartsContainer from './components/ChartsContainer.svelte';
     import Page2Charts from './Page2Charts.svelte';
+    import {default as copy} from './data/copy.json';
 
-    const subNavMenuOptions = [
-        {text: "Land Temperature", icon: 'test'},
-        {text: "Ocean Temperature", icon: 'test'},
-        {text: "Fires", icon: 'test'},
+    const sections = [
+        {
+            text: "Land Temperature", icon: 'test',
+            copy: copy.happening.surface,
+        },
+        {
+            text: "Ocean Temperature", icon: 'test',
+            copy: copy.happening.ocean,
+        },
+        {
+            text: "Fires", icon: 'test',
+            copy: copy.happening.fire,
+        },
     ];
 
-    let selectedNav = subNavMenuOptions[0];
-
+    let selectedSection = sections[0];
 
 </script>
 
-<SubNav bind:selected={selectedNav} options={subNavMenuOptions} />
+<SubNav bind:selected={selectedSection} options={sections} />
 
-{#if selectedNav === subNavMenuOptions[0]}
-<Land/>
-{:else if selectedNav === subNavMenuOptions[1]}
-<Ocean/>
-{:else if selectedNav === subNavMenuOptions[2]}
-<Fires/>
-{/if}
+
+<MainViz>
+    {#if selectedSection.text === "Land Temperature"}
+    <CopyPane>
+        <CopyHeadline>{selectedSection.copy.title}</CopyHeadline>
+        <CopyMain>{selectedSection.copy.summary}</CopyMain>
+    </CopyPane>
+    {:else if selectedSection.text === "Ocean Temperature"}
+    <CopyPane>
+        <CopyHeadline>{selectedSection.copy.title}</CopyHeadline>
+        <CopyMain>{selectedSection.copy.summary}</CopyMain>
+    </CopyPane>
+    {:else if selectedSection.text === "Fires"}
+    <CopyPane>
+        <CopyHeadline>{selectedSection.copy.title}</CopyHeadline>
+        <CopyMain>{selectedSection.copy.summary}</CopyMain>
+    </CopyPane>
+
+    {/if}
+
+
+    <div class="cartogram-container">
+        {#if selectedSection.text === "Land Temperature"}
+        <img src="surface.png" alt="Land Temperature Map" />
+        {:else if selectedSection.text === "Ocean Temperature"}
+        <img src="ocean.png" alt="Ocean Temperature Map" />
+        {:else if selectedSection.text === "Fires"}
+        <img src="fire.png" alt="Fires Map" />
+        {/if}
+    </div>
+
+
+</MainViz>
 
 <ChartsContainer>
     <Page2Charts></Page2Charts>
-
 </ChartsContainer>
 
 <style>
+    .cartogram-container > img {
+        margin-right: -100px;
+        width: 600px;
+    }
 </style>
