328c099e8ba5a88f64f5c6026368a64b9dfc7594
Matt Osborn <mattjosborn@gmailcom>
2021-03-24 10:32:25 +0000

    copy styling

--- src/components/CopyHeadline.svelte
+++ src/components/CopyHeadline.svelte
@@ -6,8 +6,10 @@
     h1 {
         color: #000;
         font-size: 38px;
+        line-height: 44px;
         font-weight: 300;
         margin-bottom: 20px;
+        margin-top: 14px;
     }
 
 </style>
