f9d2a1a45f19f4aaeb91df690e42cd1673bb91b6
Matt Osborn <mattjosborn@gmailcom>
2021-03-24 10:28:05 +0000

    carto legend

---
+++ src/maps/CartogramLegend.svelte
@@ -0,0 +1,38 @@
+<ul>
+    <li>
+        <span class="icon bg--climbing-fast"></span>
+        <span class="text">Climbing</span>
+    </li>
+    <li>
+        <span class="icon bg--falling"></span>
+        <span class="text">Decreasing</span>
+    </li>
+    <li>
+        <span class="icon bg--stable"></span>
+        <span class="text">Stable emissions</span>
+    </li>
+</ul>
+
+
+<style>
+    ul {
+        font-size: 14px;
+        display: flex;
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+    }
+    li {
+        text-indent: 0;
+        margin-right: 20px;
+    }
+    .icon {
+        display: inline-block;
+        width: 15px;
+        height: 15px;
+        margin-right: 3px;
+        border-radius: 4px;
+        margin-bottom: -2px;
+    }
+
+</style>
