e12161f58af9763a58230b245a9195892116db0f
Matt Osborn <mattjosborn@gmailcom>
2021-03-25 10:59:49 +0000

    add menu svgs

--- src/svg/index.ts
+++ src/svg/index.ts
@@ -1,3 +1,27 @@
 import test from "./test.svg";
-import downarrow from './downarrow.svg'
-export default { test, downarrow };
+import downarrow from './downarrow.svg';
+import whatshappeningMain from './icons_whats-happening--main.svg';
+import whatshappeningOcean from './icons_whats-happening--ocean.svg';
+import whatshappeningFire from './icons_whats-happening--fire.svg';
+import whatshappeningSurface from './icons_whats-happening--surface.svg';
+
+import stateoftheclimateMain from './icons_state-of-the-climate--main.svg';
+import stateoftheclimateEmissions from './icons_state-of-the-climate--total-emissions.svg';
+import stateoftheclimatePerCapita from './icons_state-of-the-climate--per-capita.svg';
+import stateoftheclimateTrends from './icons_state-of-the-climate--trends.svg';
+
+export default {
+    test, downarrow,
+    stateoftheclimate: {
+        main: stateoftheclimateMain,
+        emissions: stateoftheclimateEmissions,
+        percapita: stateoftheclimatePerCapita,
+        trends: stateoftheclimateTrends
+    },
+    whatshappening: {
+        main: whatshappeningMain,
+        ocean: whatshappeningOcean,
+        fire: whatshappeningFire,
+        surface: whatshappeningSurface,
+    }
+};
