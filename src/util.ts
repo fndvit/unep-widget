51dee7395edbc678164d2e0203ca18a85499613a
Matt Osborn <mattjosborn@gmailcom>
2021-04-11 15:07:36 +0100

    add some util functions

--- src/util.ts
+++ src/util.ts
@@ -25,3 +25,32 @@ export function trailingDebounce(fn: Function, delay: number) {
 export function clamp(val: number, min:number, max: number) {
     return Math.max(min, Math.min(max, val));
 }
+
+export function* generateRange(end: number, start = 0, step = 1) {
+    let x = start - step;
+    while(x < end - step) yield x += step;
+}
+
+export function createLookup<T, Y>
+(
+    arr: T[],
+    keyFn: (d: T) => string,
+    valFn: (d: T) => Y
+): {[key: string]: Y} {
+
+    const lookup: {[key: string]: Y} = {};
+
+    arr.forEach(d => {
+        const key = keyFn(d);
+        lookup[key] = valFn(d);
+    })
+
+    return lookup;
+}
+
+export function displayVal(val: number, dp: number = null): string {
+    if (dp === null) return val.toLocaleString();
+
+    const multiplier = Math.pow(10, dp);
+    return (Math.round(val*multiplier)/multiplier).toLocaleString()
+}
